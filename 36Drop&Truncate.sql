a39cfe3d85b9278faf16cd1fb9b6152d025febf9
aanchalsaket <aanchalsaketggits@gmail.com>
2023-09-27 12:23:18 +0530

    drop

--- 36Drop&Truncate.sql
+++ 36Drop&Truncate.sql
@@ -1,3 +1,17 @@
+CREATE TABLE DT
+(
+	Id int(2),
+    name varchar(40),
+    city varchar(30)
+);
+
+INSERT INTO DT
+VALUES
+(1, "Amit", "Agra"),
+(2, "Arpit", "Delhi");
+
+select * from DT;
+
 -- TRUNCATE 
 Truncate table dt;
 
