ff31cd8ab997bb59a0e19f005a05d7a45fed993c
aanchalsaket <aanchalsaketggits@gmail.com>
2023-09-27 12:43:14 +0530

    learned creating and alter views

---
+++ 37View.sql
@@ -0,0 +1,28 @@
+CREATE VIEW studentData
+AS 
+SELECT s.id, s.name, c.crname
+FROM student s INNER JOIN courses c
+ON s.courses = c.crid;
+
+SELECT * FROM studentData;
+
+ALTER VIEW studentData
+AS
+SELECT s.id, s.name, c.crname, ci.cityname
+FROM student s INNER JOIN courses c
+ON s.courses = c.crid
+INNER JOIN city ci
+ON s.city = ci.cid;
+
+CREATE OR REPLACE VIEW studentData
+AS
+SELECT s.id, s.name, c.crname, ci.cityname
+FROM student s INNER JOIN courses c
+ON s.courses = c.crid
+INNER JOIN city ci
+ON s.city = ci.cid;
+
+SELECT * FROM studentData;
+
+
+
